4ec7f32027860364edd0892607eb7ca51dc2e668
PV-duwesh <duwesh.kumar@PVADVISORY.IN>
2025-01-08 02:45:17 +0530

    add: MetaData configuration

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,10 +1,22 @@
-async function ProductDetail({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: Promise<{ productId: string }>;
-}) {
+};
+
+async function ProductDetails({ params }: Props) {
   const productId = (await params).productId;
   return <div>Product Detail of {productId}</div>;
 }
 
-export default ProductDetail;
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const productId = (await params).productId;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => resolve(`iPhone ${productId}`), 2000);
+  });
+  return { title: `Product ${title}` };
+};
+
+export default ProductDetails;
