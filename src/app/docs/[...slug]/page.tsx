a731fcc9d3ebf9026b04c21adcf11dd335cf63d5
PV-duwesh <duwesh.kumar@PVADVISORY.IN>
2025-01-08 01:08:07 +0530

    add: catch all segment : Docs/[slug]

---
+++ src/app/docs/[...slug]/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+async function Docs({ params }: { params: Promise<{ slug: string[] }> }) {
+  const { slug } = await params;
+
+  if (slug.length === 2) {
+    return (
+      <div>
+        Viewing docs for feature {slug[0]} and concept {slug[1]}
+      </div>
+    );
+  }
+
+  if (slug.length === 1) {
+    return <div>Viewing docs for feature {slug[0]}</div>;
+  }
+  return <div>Docs home page - {slug.join("/")}</div>;
+}
+
+export default Docs;
