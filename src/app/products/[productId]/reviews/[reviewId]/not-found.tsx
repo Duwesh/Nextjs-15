dab4d5eb3ed92c17da37846c2f1a47b9fcc81e4d
PV-duwesh <duwesh.kumar@PVADVISORY.IN>
2025-01-08 01:24:32 +0530

    add: dynamic not found, file specific

--- src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -1,7 +1,17 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
 export default function NotFound() {
+  const pathName = usePathname();
+  const productId = pathName?.split("/")[2];
+
+  const reviewId = pathName?.split("/")[4];
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800">Review Not Found</h1>
+      <h1 className="text-4xl font-bold text-gray-800">
+        Review {reviewId} not found for Product {productId}
+      </h1>
     </div>
   );
 }
