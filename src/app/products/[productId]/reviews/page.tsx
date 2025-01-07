6207fa1e2596291b50027ecbe3a1ff87d2f9ee5d
PV-duwesh <duwesh.kumar@PVADVISORY.IN>
2025-01-08 00:56:55 +0530

    add: dynamic nested routes (products/reviews)

---
+++ src/app/products/[productId]/reviews/page.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+function Reviews() {
+  return (
+    <div>
+      <section className="p-4">
+        <h1 className="text-xl font-bold underline capitalize">Reviews</h1>
+        <section className="flex flex-col justify-center">
+          {Array.from({ length: 10 }).map((_, i) => (
+            <Link
+              href={`/products/1/reviews/${i}`}
+              key={i}
+              className="text-blue-500 hover:underline"
+            >
+              <span>Review {i}</span>
+            </Link>
+          ))}
+        </section>
+      </section>
+    </div>
+  );
+}
+
+export default Reviews;
