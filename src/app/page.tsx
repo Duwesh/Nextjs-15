ea8347d92c23deb4a3a78af96765cd71c1479b25
PV-duwesh <duwesh.kumar@PVADVISORY.IN>
2025-01-08 01:59:23 +0530

    add: layout page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="">Welcom Home</h1>;
+  return <h1 className="">Welcome Home</h1>;
 }
--- src/app/products/[productId]/reviews/page.tsx
+++ src/app/products/[productId]/reviews/page.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 function Reviews() {
   return (
     <div>
-      <section className="p-4">
+      <section className="p-4 h-full w-full">
         <h1 className="text-xl font-bold underline capitalize">Reviews</h1>
         <section className="flex flex-col justify-center">
           {Array.from({ length: 10 }).map((_, i) => (
             <Link
               href={`/products/1/reviews/${i}`}
               key={i}
-              className="text-blue-500 hover:underline"
+              className="text-blue-500 hover:underline block"
             >
               <span>Review {++i}</span>
             </Link>
